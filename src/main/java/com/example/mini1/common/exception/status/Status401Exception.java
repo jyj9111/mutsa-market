6dd69c8416a23ebc9300786517a33be4cf5e9bc4
jyj9111 <mrjang9111@gmail.com>
2023-07-30 19:22:35 +0900

    refactor: 사용하지 않는 Status401Exception 클래스 삭제

--- src/main/java/com/example/mini1/common/exception/status/Status401Exception.java
+++
@@ -1,7 +0,0 @@
-package com.example.mini1.common.exception.status;
-
-public class Status401Exception extends RuntimeException{
-    public Status401Exception(String message) {
-        super(message);
-    }
-}
