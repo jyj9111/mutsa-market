064e76b4ebf2166dd9963eddd030325b6d6b8e3f
jyj9111 <mrjang9111@gmail.com>
2023-07-30 19:21:25 +0900

    fix: HttpStatus Code 401 -> 400 으로 변경

--- src/main/java/com/example/mini1/items/exception/UnAuthItemDeleteException.java
+++ src/main/java/com/example/mini1/items/exception/UnAuthItemDeleteException.java
@@ -1,8 +1,8 @@
 package com.example.mini1.items.exception;
 
-import com.example.mini1.common.exception.status.Status401Exception;
+import com.example.mini1.common.exception.status.Status400Exception;
 
-public class UnAuthItemDeleteException extends Status401Exception {
+public class UnAuthItemDeleteException extends Status400Exception {
     public UnAuthItemDeleteException() {
         super("상품 등록자만 삭제가 가능합니다.");
     }
--- src/main/java/com/example/mini1/items/exception/UnAuthItemEditException.java
+++ src/main/java/com/example/mini1/items/exception/UnAuthItemEditException.java
@@ -1,8 +1,8 @@
 package com.example.mini1.items.exception;
 
-import com.example.mini1.common.exception.status.Status401Exception;
+import com.example.mini1.common.exception.status.Status400Exception;
 
-public class UnAuthItemEditException extends Status401Exception {
+public class UnAuthItemEditException extends Status400Exception {
     public UnAuthItemEditException() {
         super("상품 등록자만 수정이 가능합니다.");
     }
