09b5262d5b9c5f18942606a70efd92cd65cd2585
jyj9111 <mrjang9111@gmail.com>
2023-07-31 22:39:02 +0900

    fix: "댓글" -> "답글" 로 변경

--- src/main/java/com/example/mini1/comments/exception/UnAuthCommentReplyException.java
+++ src/main/java/com/example/mini1/comments/exception/UnAuthCommentReplyException.java
@@ -4,6 +4,6 @@ import com.example.mini1.common.exception.status.Status400Exception;
 
 public class UnAuthCommentReplyException extends Status400Exception {
     public UnAuthCommentReplyException() {
-        super("댓글 등록자만 수정이 가능합니다.");
+        super("답글 등록자만 수정이 가능합니다.");
     }
 }
